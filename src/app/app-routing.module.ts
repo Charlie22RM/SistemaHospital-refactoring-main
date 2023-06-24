dc2594aa1b54ddb0094c13f02bd698cce68e2f3d
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-24 15:23:52 -0500

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
-
-
-
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
@@ -13,7 +10,6 @@ const routes: Routes = [
   { path: 'pacientes', loadChildren: () => import('./pacientes/pacientes.module').then(x => x.PacientesModule) },
   { path: 'medicos', loadChildren: () => import('./medicos/medicos.module').then(x => x.MedicosModule) },
   { path: 'administrador', loadChildren: () => import('./administrador/administrador.module').then(x => x.AdministradorModule) },
-
 ];
 
 @NgModule({
