c95e405c21f6ceb61d1f0cf4d9122c5559e7c043
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-24 16:07:34 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-//import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
-//import { AdministradorComponent } from './administrador/administrador.component';
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -23,7 +17,6 @@ import { LoginModule } from './login/login.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    //FormsModule,
     HttpClientModule,
     RegisterModule,
     LoginModule,
